eb78f963bcad08bdd1cb0519c2aab0cd909c597e
Muhammad Adeel <rana.adeel7273@gmail.com>
2019-09-12 19:47:29 +0500

    gameover method implemented and some validations
    gameover method implemented and some validations

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 let min = 1,
     max = 10,
     winningNumber = 2;
-    guessesLeft = 2;
+    guessesLeft = 3;
 // UI Elements
 
 const game = document.querySelector('.game'),
@@ -25,19 +25,43 @@ guessBtn.addEventListener('click', function(){
     if(isNaN(guess) || guess < min || guess > max){
         setMessage(`Please enter numbers between ${min} and ${max}`, 'red');
     }
-    // Check if won
-    if(guess === winningNumber){
-        //disable input
-        guessInput.disabled = true;
-        guessInput.style.borderColor = 'green';
-        setMessage(`${guess} is correct, YOU WIN!`, 'green');
-    }else{
-        
+    else{
+        // Check if won
+        if(guess === winningNumber){
+            //game over
+            //won
+            gameOver(true, `${guess} is correct, YOU WIN!`);
+        }else{
+            //lost
+            guessesLeft -= 1;
+            if(guessesLeft === 0){
+                //gameover
+                //lost
+                gameOver(false, `Game over, you lost. The correct number was ${winningNumber}`);
+            }else{
+                //lost 
+                //answer wrong
+                guessInput.value = '';
+                guessInput.borderColor = 'red';
+                setMessage(`${guess} is not correct. ${guessesLeft} guesses left`, 'red');
+            }
+        }
     }
+    
 });
 
 // Set Message
 function setMessage(msg, color){
     message.style.color = color;
     message.textContent = msg;
+}
+//game over
+function gameOver(won, msg){
+    let color;
+    won === true ? color = 'green': color = 'red';
+    console.log(color);
+    guessInput.disabled = true;
+    guessInput.style.borderColor = color;
+    message.style.color = color;
+    setMessage(msg);
 }
